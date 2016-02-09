35543a48583c86b75dc3259d557322faff56eb1c
lolkaenot <lolkaenot@gmail.com>
2016-02-09 11:23:01 +0300

    работа с новостями из бд

---
+++ www/model/news.php
@@ -0,0 +1,31 @@
+<?php
+require __DIR__ . '/../function/sql.php';
+
+/** Получить все новости отсортированные по дате в обратном порядке */
+function news_getAll(){
+
+    $str = 'SELECT * FROM news ORDER BY date DESC';
+    return sql_getAll($str);
+}
+
+/** Удалить новость по ее id */
+function news_delete($id){
+    $str = 'DELETE FROM news WHERE id=' . $id;
+    sql_request($str);
+}
+
+/** Добавть новость */
+function news_add($title, $article){
+    $date = date("Y.m.d H:i:s");
+    $str = "INSERT INTO news (date, title, article)
+            VALUES ('" . $date . "', '" . $title . "', '" . $article . "')";
+    sql_request($str);
+}
+
+/** Изменить новость по ее id */
+function news_edit($id, $title, $article){
+    $str = "UPDATE news
+            SET title='" . $title . "', article='" . $article . "'
+            WHERE id='". $id . "'";
+    sql_request($str);
+}
